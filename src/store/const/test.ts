ad533c707321d21bea85a6913e380309776d89b4
Дмитрий <dmitriy.golovin.2001.03@mail.ru>
2026-07-08 12:26:46 +0300

    попытка изменить работу testReducerByChange что бы initialState бралься ис предыдушего state

--- src/store/const/test.ts
+++ src/store/const/test.ts
@@ -1,10 +1,16 @@
 import { it, test, expect } from 'vitest';
-import { TAction } from './test-type.ts';
+import type { Reducer } from '@reduxjs/toolkit';
+import { TReducerAction, TReducerActionFromStrict } from './test-type.ts';
 import { TObject } from '../../types/types.ts';
 
-let objs: Array<[TObject | Array<unknown>, TObject | Array<unknown>]> = [];
-const isObject = (value: unknown): value is object => typeof value === 'object' && value !== null;
-const copyObjSuperficial = (obj1: TObject, obj2: TObject): void => {
+const isObject = (value: unknown): value is object =>
+  typeof value === 'object' && value !== null;
+type TObjs = Array<[TObject | Array<unknown>, TObject | Array<unknown>]>;
+const copyObjSuperficial = (
+  obj1: TObject,
+  obj2: TObject,
+  objs: TObjs,
+): void => {
   for (const prop in obj2) {
     const value1 = obj1[prop];
     const value2 = obj2[prop];
@@ -26,47 +32,46 @@ const copyObjSuperficial = (obj1: TObject, obj2: TObject): void => {
     objs.push([obj1[prop], obj2[prop]]);
   }
 };
-const copyObjReducer = <TState extends object>(
+const copyObjReducer = <TState extends TObject>(
   obj1: TState,
-  obj2: object,
+  obj2: TObject,
 ): TState => {
-  objs = [];
-  copyObjSuperficial(obj1, obj2);
+  const objs: TObjs = [];
+  const copyObjThis = (obj1: TObject, obj2: TObject) =>
+    copyObjSuperficial(obj1, obj2, objs);
+  copyObjThis(obj1, obj2);
   while (objs.length) {
-    type TObjsArr = [object, object];
+    type TObjsArr = [TObject, TObject];
     const [obj1, obj2] = objs.shift() as TObjsArr;
-    copyObjSuperficial(obj1, obj2);
+    copyObjThis(obj1, obj2);
   }
   return obj1;
 };
 
-type TTestAction = (
-  text: string,
-  fun: (param: unknown) => TAction,
-  type: string,
-  parameters: Array<unknown>,
-) => void;
-export const testAction: TTestAction = (text, fun, type, parameters) => {
-  test.each(parameters)(`${text}: %j`, (param) => {
-    const objSetCity = fun(param);
-    expect(objSetCity).toEqual({ type, payload: param });
-  });
-};
-export const testActionLink: TTestAction = (text, fun, type, parameters) => {
-  test.each(parameters)(`${text}: %j`, (param) => {
-    const objSetCity = fun(param);
-    expect(objSetCity).toEqual({ type, payload: param });
-  });
-  test.each(parameters)(`${text} link ===: %j`, (param) => {
-    const objSetCity = fun(param);
-    expect(objSetCity.payload).toBe(param);
-  });
-};
+// type TTestAction = (
+//   text: string,
+//   fun: (param: unknown) => TAction,
+//   type: string,
+//   parameters: Array<unknown>,
+// ) => void;
+// export const testAction: TTestAction = (text, fun, type, parameters) => {
+//   test.each(parameters)(`${text}: %j`, (param) => {
+//     const objSetCity = fun(param);
+//     expect(objSetCity).toEqual({ type, payload: param });
+//   });
+// };
+// export const testActionLink: TTestAction = (text, fun, type, parameters) => {
+//   test.each(parameters)(`${text}: %j`, (param) => {
+//     const objSetCity = fun(param);
+//     expect(objSetCity).toEqual({ type, payload: param });
+//   });
+//   test.each(parameters)(`${text} link ===: %j`, (param) => {
+//     const objSetCity = fun(param);
+//     expect(objSetCity.payload).toBe(param);
+//   });
+// };
 
-export type TReducer<TState extends TObject> = (
-  state: TState | undefined,
-  action: TAction,
-) => TState;
+export type TReducer<TState extends TObject> = Reducer<TState>;
 
 export type NarrowFromExpect<
   TState extends TObject,
@@ -88,36 +93,36 @@ type LastExpectFromParams<
   ? TLast
   : Partial<TState>;
 
-type TActionExpects<TState extends TObject> =
-  | [TAction, TState]
-  | [TAction, TState, ...Array<TAction | TState>];
-type TActionExpectsPartial<TState extends TObject> = TActionExpects<
-  Partial<TState>
->;
+type TActionExpects<TState extends TObject, TAction = TReducerAction> =
+  | [string,TAction, TState]
+  | [string,TAction, TState, ...Array<TAction | TState | string>];
+type TActionExpectsPartial<
+  TState extends TObject,
+  TAction = TReducerAction,
+> = TActionExpects<Partial<TState>, TAction>;
 
 export const testReducer = <
   TState extends TObject,
   TParams extends TActionExpectsPartial<TState> = TActionExpectsPartial<TState>,
 >(
-  text: string,
-  initialState: TState | undefined,
-  reducer: TReducer<TState>,
-  ...lastParams: TActionExpects<TState>
-): NarrowFromExpect<TState, LastExpectFromParams<TState, TParams>> => {
-  type TCallback = (state: TState, expectState: TState) => void;
-  const lastParamsFor = (callback: TCallback = () => {}):TState => {
-    let state: TState;
-    for (let i = 0; i < lastParams.length - 1; i += 2) {
-      type TArrActionExpectstate = [TAction, TState];
-      const [action, expectState] = lastParams.slice(
+    initialState: TState | undefined,
+    reducer: TReducer<TState>,
+    ...lastParams: TActionExpects<TState>
+  ): NarrowFromExpect<TState, LastExpectFromParams<TState, TParams>> => {
+  type TCallback = (text:string,state: TState, expectState: TState) => void;
+  const lastParamsFor = (callback: TCallback = () => {}): TState => {
+    let state: TState | undefined = initialState;
+    for (let i = 0; i < lastParams.length - 1; i += 3) {
+      type TArrActionExpectstate = [string,TReducerAction, TState];
+      const [text, action, expectState] = lastParams.slice(
         i,
-        i + 2,
+        i + 3,
       ) as TArrActionExpectstate;
-      if (initialState !== undefined) {
-        initialState = copyObjReducer({...initialState}, initialState);
+      if (state !== undefined) {
+        state = copyObjReducer({ ...state }, state);
       }
-      state = reducer(initialState, action);
-      callback(state, expectState);
+      state = reducer(state, action);
+      callback(text, state, expectState);
     }
     if (state === undefined) {
       throw new Error(
@@ -126,8 +131,9 @@ export const testReducer = <
     }
     return state;
   };
-  it(`${text} reducer`, () => {
-    lastParamsFor((state, expectState) => {
+
+  lastParamsFor((text, state, expectState) => {
+    it(`${text} reducer`, () => {
       expect(state).toEqual(expectState);
     });
   });
@@ -138,85 +144,79 @@ export const testReducerByChange = <
   TState extends TObject,
   TParams extends TActionExpectsPartial<TState> = TActionExpectsPartial<TState>,
 >(
-  text: string,
-  initialState: TState | undefined,
-  reducer: TReducer<TState>,
-  ...lastParams: TParams
-): NarrowFromExpect<TState, LastExpectFromParams<TState, TParams>> => {
-  if (typeof initialState === 'object') {
-    for (let i = 1; i < lastParams.length; i += 2) {
-      const initialStateCopy = { ...initialState };
-      copyObjReducer(initialStateCopy, lastParams[i]);
-      lastParams[i] = initialStateCopy;
-    }
-  }
-  const lastParamsFinal = lastParams as TActionExpects<TState>;
+    initialState: TState | undefined,
+    reducer: TReducer<TState>,
+    ...lastParams: TParams
+  ): NarrowFromExpect<TState, LastExpectFromParams<TState, TParams>> => {
   return testReducer<TState, TParams>(
-    text,
     initialState,
     reducer,
-    ...lastParamsFinal,
+    ...lastParams,
   );
 };
+
+const unknownReducerAction: TReducerAction = { type: '@@TEST/UNKNOWN' };
 
 export const testReduxUndefined = <TState extends TObject>(
   text: string,
   reducer: TReducer<TState>,
   expectState: TState,
 ): TState =>
-  testReducer<TState>(
-    `${text} default`,
-    undefined,
-    reducer,
-    { type: '', payload: { email: '' } },
-    expectState,
-  );
+    testReducer(
+      undefined,
+      reducer,
+      `${text} default`,
+      unknownReducerAction,
+      expectState,
+    );
 
 export type TTestSpecificRedux = (
   text: string,
   initialState: object,
-  action: TAction,
+  action: TReducerAction,
   expect: object,
 ) => object;
 
-type TTestReducerParam<TState extends TObject> = (
-  text: string,
+type TTestReducerParam<TState extends TObject, TAction = TReducerAction> = (
   initialState: TState | undefined,
-  ...lastProps: TActionExpects<TState>
+  ...lastProps: TActionExpects<TState, TAction>
 ) => TState;
 
-type TTestReducerByChangeParam<TState extends TObject> = <
-  TParams extends TActionExpectsPartial<TState>,
->(
-  text: string,
+type TTestReducerByChangeParam<
+  TState extends TObject,
+  TAction = TReducerAction,
+> = <TParams extends TActionExpectsPartial<TState, TAction>>(
   initialState: TState | undefined,
   ...lastProps: TParams
 ) => NarrowFromExpect<TState, LastExpectFromParams<TState, TParams>>;
 
-type TTest<TState extends TObject = TObject> = (
-  testReducer: TTestReducerParam<TState>,
-  testReducerByChange: TTestReducerByChangeParam<TState>,
+type TTest<TState extends TObject = TObject, TAction = TReducerAction> = (
+  testReducer: TTestReducerParam<TState, TAction>,
+  testReducerByChange: TTestReducerByChangeParam<TState, TAction>,
 ) => void;
 
-export const testDescribe = <TState extends TObject>(
-  text: string,
-  reducer: TReducer<TState>,
-  expectState: TState,
-  test: TTest<TState>,
-): void => {
-  const testSpecificReducer: TTestReducerParam<TState> = (
-    text,
-    initialState,
-    ...lastProps
-  ) => testReducer<TState>(text, initialState, reducer, ...lastProps);
+export const testDescribe = <
+  TState extends TObject,
+  TStrictAction extends boolean = true,
+>(
+    text: string,
+    reducer: TReducer<TState>,
+    expectState: TState,
+    test: TTest<TState, TReducerActionFromStrict<TStrictAction>>,
+  ): void => {
+  const testSpecificReducer: TTestReducerParam<
+    TState,
+    TReducerActionFromStrict<TStrictAction>
+  > = (initialState, ...lastProps) =>
+    testReducer<TState>(initialState, reducer, ...lastProps);
 
-  const testSpecificReducerByChange: TTestReducerByChangeParam<TState> = (
-    text,
-    initialState,
-    ...lastProps
-  ) => testReducerByChange(text, initialState, reducer, ...lastProps);
+  const testSpecificReducerByChange: TTestReducerByChangeParam<
+    TState,
+    TReducerActionFromStrict<TStrictAction>
+  > = (initialState, ...lastProps) =>
+    testReducerByChange(initialState, reducer, ...lastProps);
   describe(text, () => {
-    testReduxUndefined<TState>('', reducer, expectState);
+    testReduxUndefined('', reducer, expectState);
     test(testSpecificReducer, testSpecificReducerByChange);
   });
 };
--- src/store/offersSlice/offersSlice.test.ts
+++ src/store/offersSlice/offersSlice.test.ts
@@ -5,11 +5,269 @@ import { initialStateOffers, TStateOffers } from './offersSlice.ts';
 import { TOffers } from '../../types/types.ts';
 
 const { reducer } = offersSlice;
+// testDescribe<TStateOffers>(
+//   'offersSlice',
+//   reducer,
+//   initialStateOffers,
+//   (_, testReducerByChange) => {
+//     // it('offersSlice reducer',() => {
+//     //   const city = 'london';
+//     //   const cityAction = setCity(city);
+//     //   const initialState = {
+//     //     city: 'Paris',
+//     //     offersByCitie: null
+//     //   };
+//     //   const state = offersSlice.reducer({...initialState}, cityAction);
+//     //   expect(state).toEqual({ ...initialState, city });
+//     // });
+//     const props = 'Amsterdam';
+//     testReducerByChange('setCity', initialStateOffers, setCity(props), {
+//       city: props,
+//     });
+//     let offer: TOffers = {
+//       id: '82fc8d67-a851-4637-964f-88fe508e833c',
+//       title: 'Tile House',
+//       type: 'hotel',
+//       price: 417,
+//       previewImage: 'https://16.design.htmlacademy.pro/static/hotel/12.jpg',
+//       city: {
+//         name: 'Paris',
+//         location: {
+//           latitude: 48.85661,
+//           longitude: 2.351499,
+//           zoom: 13,
+//         },
+//       },
+//       location: {
+//         latitude: 48.868610000000004,
+//         longitude: 2.342499,
+//         zoom: 16,
+//       },
+//       isFavorite: false,
+//       isPremium: true,
+//       rating: 2.9,
+//     };
+//     let state = testReducerByChange(
+//       'setOffer',
+//       initialStateOffers,
+//       setOffers(offer),
+//       {
+//         offersByCities: {
+//           Paris: {
+//             '82fc8d67-a851-4637-964f-88fe508e833c': offer,
+//           },
+//         },
+//       },
+//     );
+
+//     offer = {
+//       id: 'cced6fa8-419a-4fde-9702-2a6c81dda568',
+//       title: 'Nice, cozy, warm big bed apartment',
+//       type: 'hotel',
+//       price: 105,
+//       previewImage: 'https://16.design.htmlacademy.pro/static/hotel/9.jpg',
+//       city: {
+//         name: 'Paris',
+//         location: {
+//           latitude: 48.85661,
+//           longitude: 2.351499,
+//           zoom: 13,
+//         },
+//       },
+//       location: {
+//         latitude: 48.834610000000005,
+//         longitude: 2.335499,
+//         zoom: 16,
+//       },
+//       isFavorite: false,
+//       isPremium: true,
+//       rating: 4.6,
+//     };
+//     state = testReducerByChange(
+//       'setOffer 2 offers in Paris',
+//       state,
+//       setOffers(offer),
+//       {
+//         offersByCities: {
+//           Paris: {
+//             'cced6fa8-419a-4fde-9702-2a6c81dda568': offer,
+//           },
+//         },
+//       },
+//     );
+
+//     offer = {
+//       id: '00720bc9-dd78-4384-9c5b-9f70359e807d',
+//       title: 'Canal View Prinsengracht',
+//       type: 'hotel',
+//       price: 315,
+//       previewImage: 'https://16.design.htmlacademy.pro/static/hotel/2.jpg',
+//       city: {
+//         name: 'Cologne',
+//         location: {
+//           latitude: 50.938361,
+//           longitude: 6.959974,
+//           zoom: 13,
+//         },
+//       },
+//       location: {
+//         latitude: 50.950361,
+//         longitude: 6.961974,
+//         zoom: 16,
+//       },
+//       isFavorite: false,
+//       isPremium: false,
+//       rating: 1.6,
+//     };
+//     state = testReducerByChange(
+//       'setOffer Cologne',
+//       state,
+//       setOffers(offer),
+//       {
+//         offersByCities: {
+//           Cologne: {
+//             '00720bc9-dd78-4384-9c5b-9f70359e807d': offer,
+//           },
+//         },
+//       },
+//     );
+
+//     offer = {
+//       id: '00720bc9-dd78-4384-9c5b-9f70359e807d',
+//       title: 'Canal View Prinsengracht',
+//       type: 'hotel',
+//       price: 300,
+//       previewImage: 'https://16.design.htmlacademy.pro/static/hotel/2.jpg',
+//       city: {
+//         name: 'Cologne',
+//         location: {
+//           latitude: 50.938361,
+//           longitude: 6.959974,
+//           zoom: 13,
+//         },
+//       },
+//       location: {
+//         latitude: 50.950361,
+//         longitude: 6.961974,
+//         zoom: 16,
+//       },
+//       isFavorite: false,
+//       isPremium: false,
+//       rating: 1.6,
+//     };
+//     state = testReducerByChange(
+//       'setOffer Cologne change',
+//       state,
+//       setOffers(offer),
+//       {
+//         offersByCities: {
+//           Cologne: {
+//             '00720bc9-dd78-4384-9c5b-9f70359e807d': offer,
+//           },
+//         },
+//       },
+//     );
+//   },
+// );
+const offers: TOffers[] = [
+  {
+    id: '82fc8d67-a851-4637-964f-88fe508e833c',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 417,
+    previewImage: 'https://16.design.htmlacademy.pro/static/hotel/12.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13,
+      },
+    },
+    location: {
+      latitude: 48.868610000000004,
+      longitude: 2.342499,
+      zoom: 16,
+    },
+    isFavorite: false,
+    isPremium: true,
+    rating: 2.9,
+  },
+  {
+    id: 'cced6fa8-419a-4fde-9702-2a6c81dda568',
+    title: 'Nice, cozy, warm big bed apartment',
+    type: 'hotel',
+    price: 105,
+    previewImage: 'https://16.design.htmlacademy.pro/static/hotel/9.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13,
+      },
+    },
+    location: {
+      latitude: 48.834610000000005,
+      longitude: 2.335499,
+      zoom: 16,
+    },
+    isFavorite: false,
+    isPremium: true,
+    rating: 4.6,
+  },
+  {
+    id: '00720bc9-dd78-4384-9c5b-9f70359e807d',
+    title: 'Canal View Prinsengracht',
+    type: 'hotel',
+    price: 315,
+    previewImage: 'https://16.design.htmlacademy.pro/static/hotel/2.jpg',
+    city: {
+      name: 'Cologne',
+      location: {
+        latitude: 50.938361,
+        longitude: 6.959974,
+        zoom: 13,
+      },
+    },
+    location: {
+      latitude: 50.950361,
+      longitude: 6.961974,
+      zoom: 16,
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 1.6,
+  },
+  {
+    id: '00720bc9-dd78-4384-9c5b-9f70359e807d',
+    title: 'Canal View Prinsengracht',
+    type: 'hotel',
+    price: 300,
+    previewImage: 'https://16.design.htmlacademy.pro/static/hotel/2.jpg',
+    city: {
+      name: 'Cologne',
+      location: {
+        latitude: 50.938361,
+        longitude: 6.959974,
+        zoom: 13,
+      },
+    },
+    location: {
+      latitude: 50.950361,
+      longitude: 6.961974,
+      zoom: 16,
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 1.6,
+  },
+];
 testDescribe<TStateOffers>(
   'offersSlice',
   reducer,
   initialStateOffers,
-  (_, testOffersReducerByChange) => {
+  (_, testReducerByChange) => {
     // it('offersSlice reducer',() => {
     //   const city = 'london';
     //   const cityAction = setCity(city);
@@ -21,7 +279,7 @@ testDescribe<TStateOffers>(
     //   expect(state).toEqual({ ...initialState, city });
     // });
     const props = 'Amsterdam';
-    testOffersReducerByChange('setCity', initialStateOffers, setCity(props), {
+    testReducerByChange('setCity', initialStateOffers, setCity(props), {
       city: props,
     });
     let offer: TOffers = {
@@ -47,125 +305,152 @@ testDescribe<TStateOffers>(
       isPremium: true,
       rating: 2.9,
     };
-    let state = testOffersReducerByChange(
-      'setOffer',
+    let state = testReducerByChange(
       initialStateOffers,
-      setOffers(offer),
+      'setOffer',
+      setOffers(offers[0]),
       {
         offersByCities: {
           Paris: {
-            '82fc8d67-a851-4637-964f-88fe508e833c': offer,
+            '82fc8d67-a851-4637-964f-88fe508e833c': offers[0],
           },
         },
       },
-    );
-
-    offer = {
-      id: 'cced6fa8-419a-4fde-9702-2a6c81dda568',
-      title: 'Nice, cozy, warm big bed apartment',
-      type: 'hotel',
-      price: 105,
-      previewImage: 'https://16.design.htmlacademy.pro/static/hotel/9.jpg',
-      city: {
-        name: 'Paris',
-        location: {
-          latitude: 48.85661,
-          longitude: 2.351499,
-          zoom: 13,
-        },
-      },
-      location: {
-        latitude: 48.834610000000005,
-        longitude: 2.335499,
-        zoom: 16,
-      },
-      isFavorite: false,
-      isPremium: true,
-      rating: 4.6,
-    };
-    state = testOffersReducerByChange(
       'setOffer 2 offers in Paris',
-      state,
-      setOffers(offer),
+      setOffers(offers[1]),
       {
         offersByCities: {
           Paris: {
-            'cced6fa8-419a-4fde-9702-2a6c81dda568': offer,
+            'cced6fa8-419a-4fde-9702-2a6c81dda568': offers[1],
           },
         },
       },
-    );
-
-    offer = {
-      id: '00720bc9-dd78-4384-9c5b-9f70359e807d',
-      title: 'Canal View Prinsengracht',
-      type: 'hotel',
-      price: 315,
-      previewImage: 'https://16.design.htmlacademy.pro/static/hotel/2.jpg',
-      city: {
-        name: 'Cologne',
-        location: {
-          latitude: 50.938361,
-          longitude: 6.959974,
-          zoom: 13,
-        },
-      },
-      location: {
-        latitude: 50.950361,
-        longitude: 6.961974,
-        zoom: 16,
-      },
-      isFavorite: false,
-      isPremium: false,
-      rating: 1.6,
-    };
-    state = testOffersReducerByChange(
       'setOffer Cologne',
-      state,
-      setOffers(offer),
+      setOffers(offers[2]),
       {
         offersByCities: {
           Cologne: {
-            '00720bc9-dd78-4384-9c5b-9f70359e807d': offer,
+            '00720bc9-dd78-4384-9c5b-9f70359e807d': offers[2],
+          },
+        },
+      },
+      'setOffer Cologne change',
+      setOffers(offers[2]),
+      {
+        offersByCities: {
+          Cologne: {
+            '00720bc9-dd78-4384-9c5b-9f70359e807d': offers[3],
           },
         },
       },
     );
 
-    offer = {
-      id: '00720bc9-dd78-4384-9c5b-9f70359e807d',
-      title: 'Canal View Prinsengracht',
-      type: 'hotel',
-      price: 300,
-      previewImage: 'https://16.design.htmlacademy.pro/static/hotel/2.jpg',
-      city: {
-        name: 'Cologne',
-        location: {
-          latitude: 50.938361,
-          longitude: 6.959974,
-          zoom: 13,
-        },
-      },
-      location: {
-        latitude: 50.950361,
-        longitude: 6.961974,
-        zoom: 16,
-      },
-      isFavorite: false,
-      isPremium: false,
-      rating: 1.6,
-    };
-    state = testOffersReducerByChange(
-      'setOffer Cologne change',
-      state,
-      setOffers(offer),
-      {
-        offersByCities: {
-          Cologne: {
-            '00720bc9-dd78-4384-9c5b-9f70359e807d': offer,
-          },
-        },
-      },
-    );
+    // offer = {
+    //   id: 'cced6fa8-419a-4fde-9702-2a6c81dda568',
+    //   title: 'Nice, cozy, warm big bed apartment',
+    //   type: 'hotel',
+    //   price: 105,
+    //   previewImage: 'https://16.design.htmlacademy.pro/static/hotel/9.jpg',
+    //   city: {
+    //     name: 'Paris',
+    //     location: {
+    //       latitude: 48.85661,
+    //       longitude: 2.351499,
+    //       zoom: 13,
+    //     },
+    //   },
+    //   location: {
+    //     latitude: 48.834610000000005,
+    //     longitude: 2.335499,
+    //     zoom: 16,
+    //   },
+    //   isFavorite: false,
+    //   isPremium: true,
+    //   rating: 4.6,
+    // };
+    // state = testReducerByChange(
+    //   'setOffer 2 offers in Paris',
+    //   state,
+    //   setOffers(offer),
+    //   {
+    //     offersByCities: {
+    //       Paris: {
+    //         'cced6fa8-419a-4fde-9702-2a6c81dda568': offer,
+    //       },
+    //     },
+    //   },
+    // );
+
+    // offer = {
+    //   id: '00720bc9-dd78-4384-9c5b-9f70359e807d',
+    //   title: 'Canal View Prinsengracht',
+    //   type: 'hotel',
+    //   price: 315,
+    //   previewImage: 'https://16.design.htmlacademy.pro/static/hotel/2.jpg',
+    //   city: {
+    //     name: 'Cologne',
+    //     location: {
+    //       latitude: 50.938361,
+    //       longitude: 6.959974,
+    //       zoom: 13,
+    //     },
+    //   },
+    //   location: {
+    //     latitude: 50.950361,
+    //     longitude: 6.961974,
+    //     zoom: 16,
+    //   },
+    //   isFavorite: false,
+    //   isPremium: false,
+    //   rating: 1.6,
+    // };
+    // state = testReducerByChange(
+    //   'setOffer Cologne',
+    //   state,
+    //   setOffers(offer),
+    //   {
+    //     offersByCities: {
+    //       Cologne: {
+    //         '00720bc9-dd78-4384-9c5b-9f70359e807d': offer,
+    //       },
+    //     },
+    //   },
+    // );
+
+    // offer = {
+    //   id: '00720bc9-dd78-4384-9c5b-9f70359e807d',
+    //   title: 'Canal View Prinsengracht',
+    //   type: 'hotel',
+    //   price: 300,
+    //   previewImage: 'https://16.design.htmlacademy.pro/static/hotel/2.jpg',
+    //   city: {
+    //     name: 'Cologne',
+    //     location: {
+    //       latitude: 50.938361,
+    //       longitude: 6.959974,
+    //       zoom: 13,
+    //     },
+    //   },
+    //   location: {
+    //     latitude: 50.950361,
+    //     longitude: 6.961974,
+    //     zoom: 16,
+    //   },
+    //   isFavorite: false,
+    //   isPremium: false,
+    //   rating: 1.6,
+    // };
+    // state = testReducerByChange(
+    //   'setOffer Cologne change',
+    //   state,
+    //   setOffers(offer),
+    //   {
+    //     offersByCities: {
+    //       Cologne: {
+    //         '00720bc9-dd78-4384-9c5b-9f70359e807d': offer,
+    //       },
+    //     },
+    //   },
+    // );
   },
 );
